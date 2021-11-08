2563827e71af526654843517f04b4a37ae12dcec
Rick <ricksmycodeguy@gmail.com>
2021-11-08 14:12:34 -0600

    wip

--- Homework/Task3.py
+++ Homework/Task3.py
@@ -12,24 +12,24 @@ l2.next = l3
 l1.next.next.next = l4 # same ast l3.next=l4
 """
 # Singly-linked lists are already defined with this interface:
-class ListNode(object):
-    def __init__(self, x):
-        self.value = x
-        self.next = None
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.value = x
+#         self.next = None
 
 
 
 
 
 def insertValueIntoSortedLinkedList(l, value):
-    pass
+    
 
 
-print(insertValueIntoSortedLinkedList([1, 3, 4, 5, 6], 5)) # [1, 3, 4, 5, 6]
-print(insertValueIntoSortedLinkedList([1, 3, 4, 6], 10)) # [1, 3, 4, 6, 10]
-print(insertValueIntoSortedLinkedList([1, 3, 4, 6], 0)) # [0, 1, 3, 4, 6]
-print(insertValueIntoSortedLinkedList([], 239)) # [239]
-print(insertValueIntoSortedLinkedList([239], 240)) #[239, 240]
-print(insertValueIntoSortedLinkedList([1, 3, 4], -100)) #[-100, 1, 3, 4]
-print(insertValueIntoSortedLinkedList([1, 3, 6, 7], 4)) # [1, 3, 4, 6, 7]
-print(insertValueIntoSortedLinkedList([-1000000000, 999999999], 1000000000)) #[-1000000000, 999999999, 1000000000]
+# print(insertValueIntoSortedLinkedList([1, 3, 4, 5, 6], 5)) # [1, 3, 4, 5, 6]
+# print(insertValueIntoSortedLinkedList([1, 3, 4, 6], 10)) # [1, 3, 4, 6, 10]
+# print(insertValueIntoSortedLinkedList([1, 3, 4, 6], 0)) # [0, 1, 3, 4, 6]
+# print(insertValueIntoSortedLinkedList([], 239)) # [239]
+# print(insertValueIntoSortedLinkedList([239], 240)) #[239, 240]
+# print(insertValueIntoSortedLinkedList([1, 3, 4], -100)) #[-100, 1, 3, 4]
+# print(insertValueIntoSortedLinkedList([1, 3, 6, 7], 4)) # [1, 3, 4, 6, 7]
+# print(insertValueIntoSortedLinkedList([-1000000000, 999999999], 1000000000)) #[-1000000000, 999999999, 1000000000]
---
+++ Homework/Task3AltClean.py
@@ -0,0 +1,19 @@
+def insertValueIntoSortedLinkedList(l, value):
+    node = ListNode(value)
+    if l == None:
+        return node
+    else:
+        if l.value > value:
+            node.next = l
+            return node
+        else:
+            cur, prev = l, None
+            while cur.next and cur.value <= value:
+                prev = cur
+                cur = cur.next
+            if cur.next == None and cur.value <= value:
+                cur.next = node
+            else:
+                node.next = prev.next
+                prev.next = node
+            return l
